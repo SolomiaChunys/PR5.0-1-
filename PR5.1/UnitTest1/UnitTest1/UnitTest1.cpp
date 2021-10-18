349a03ca39c3726ac391cc99f7bbc9a135b06ca5
Solomiia <solomiia.chunys.ik.2021@lpnu.ua>
2021-10-19 00:26:11 +0300

    spotify test

---
+++ PR5.1/UnitTest1/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "c:\соломія чунис\Політехніка\Алгоритмізація та програмування\lab№5.1\pr5.1\PR5.1\PR5.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int a = 0, b = 0;
+			int t;
+			t = (a * a) + (b * b);
+			Assert::AreEqual(t, 0);
+		}
+	};
+}
